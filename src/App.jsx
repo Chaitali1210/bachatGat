3b48b4264f00c60cfa1eaf6c9af6e1d5562da90e
Rutuja <rutujajadhav896@gmail.com>
2025-01-27 17:33:12 +0530

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import AdminPanel from './components/AdminPanel'
+import { Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
+import AdminPanel from './components/AdminPanel';
 
 const App = () => {
   return (
     <>
-      <AdminPanel />
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/admin' element={<AdminPanel />} />
+      </Routes>
     </>
   )
 }
